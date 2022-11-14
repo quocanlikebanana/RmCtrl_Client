e3428a4547cba8a2d45700cf8a6e18e8b9748c33
Boublet <65109951+Boublet@users.noreply.github.com>
2022-11-14 13:09:35 +0700

    Client_App_Ver1.0

---
+++ src/client/Client_Application.java
@@ -0,0 +1,51 @@
+package client;
+
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Client_Application {
+	
+	private ImageIcon icon = new ImageIcon("Assets/Icon/conho.png");
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Client_Application window = new Client_Application();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Client_Application() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 600, 400);
+		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+		frame.setTitle("RemoteControl Client");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setIconImage(icon.getImage());
+		
+		
+	}
+
+}
